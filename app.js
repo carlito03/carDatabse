f57814f50fb2607acd08ce0fbcc73ddc966f7791
carlito03 <carlos@ideasplusactions.com>
2017-09-19 18:01:15 +0000

    app js with mongo

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/cars");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
-  var cars = [
-        {name:"Car 1", image:"http://www.photosforclass.com/download/6532389979"},
-         {name:"Car 2", image:"https://farm9.staticflickr.com/8041/8021211725_d7f595b53d.jpg"},
-          {name:"Car 3", image:"https://farm7.staticflickr.com/6113/6277318013_c750b0c828.jpg"},
-           {name:"Car 4", image:"https://farm7.staticflickr.com/6113/6277318013_c750b0c828.jpg"}
-        ];
+//schema setup
+
+var carSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description:String
+});
+
+var Car = mongoose.model("MyCars", carSchema);
 
 app.get("/", function(req,res){
-    
+    //get all cars
+
     res.render("landing");
+   
+   //
 });
 
 app.get("/cars", function(req, res){
-    
-   
-    res.render("cars", {cars:cars});
+    // get all the cars
+     Car.find({}, function (err,allCars) {
+        // body...
+        if(err){
+            console.log(err);
+        }else {
+             res.render("index", {cars:allCars});
+        }
+    });
 });
 
 
@@ -29,16 +44,36 @@ app.post("/cars", function (req,res) {
     
     var name =  req.body.name;
     var image = req.body.image;
-    var newCar = {name: name, image:image};
+    var description = req.body.description;
+    var newCar = {name: name, image:image, description:description};
     
-    cars.push(newCar);
-    //redirect to cars page
-    res.redirect("/cars");
+    // Create new car and save to DB
     
+    Car.create(newCar, function (err, newlyCreated) {
+        // body...
+        if(err){
+            console.log(err)
+        }else {
+            res.redirect("/cars")
+        }
+    });
 });
 
 app.get("/cars/new", function(req, res) {
     res.render("new.ejs");
+});
+
+app.get("/cars/:id", function(req, res) {
+    //find car by provided ID
+    Car.findById(req.params.id, function(err, foundCar){
+        if(err){
+            console.log(err);
+        }else{
+            //render show template with that ID 
+            res.render("show", {car: foundCar});
+        }
+        
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
